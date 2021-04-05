d165eb4928a600f79634cfcf83cddaecc12b6e9d
johncrawford <jtcrawford4@gmail.com>
2021-04-05 08:51:49 -0500

    appointment date defaults to tomorrow

--- FreshMasters/Model/Customer.swift
+++ FreshMasters/Model/Customer.swift
@@ -13,7 +13,7 @@ final class Customer: ObservableObject{
     @Published var lastName = ""
     @Published var phone = ""
     @Published var email = ""
-    @Published var appointmentDate = Date()
+    @Published var appointmentDate = Calendar.current.date(byAdding: .day, value: 1, to: Date())!
     @Published var contactByEmail = false
     @Published var contactByPhone = false
     @Published var contactByText = false
